c70b32d5e928592e89a211edfefaf5435ea79dca
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-19 14:48:20 +0100

    created authorization middleware

---
+++ server/src/middlewares/authentication.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import jsonwebtoken, { JsonWebTokenError, JwtPayload } from "jsonwebtoken";
+
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) res.sendStatus(401);
+  else
+    jsonwebtoken.verify(
+      token,
+      process.env.JWT_SECRET,
+      (err: JsonWebTokenError, decoded: JwtPayload) => {
+        if (err) res.sendStatus(403);
+        else {
+          const userID = req.params.userID;
+          if (userID === null || userID === decoded.userID) next();
+          else res.sendStatus(403);
+        }
+      }
+    );
+};
