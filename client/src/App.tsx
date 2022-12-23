ef0c3c1d31ac7074f4a4bb36ab9d672c7ba1f7a8
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-23 17:39:41 +0100

    refactored App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { Button } from "./components/Button";
 import { Form } from "./components/Form";
-import { styled } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import { Tile } from "./components/Tile";
 import { ButtonDiv } from "./components/ButtonDiv";
