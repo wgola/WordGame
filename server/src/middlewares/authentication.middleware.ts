33a388317b9e20dc3027e7356e07c1f3213a7261
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-23 17:49:41 +0100

    refactored authentication middleware

--- server/src/middlewares/authentication.middleware.ts
+++ server/src/middlewares/authentication.middleware.ts
@@ -4,19 +4,19 @@ import jsonwebtoken, { JsonWebTokenError, JwtPayload } from "jsonwebtoken";
 const auth = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies["jwt-token"];
   if (token === undefined) res.sendStatus(401);
-  else
-    jsonwebtoken.verify(
-      token,
-      process.env.JWT_SECRET,
-      (err: JsonWebTokenError, decoded: JwtPayload) => {
-        if (err) res.sendStatus(403);
-        else {
-          const userID = req.params.userID;
-          if (userID === undefined || userID === decoded.userID) next();
-          else res.sendStatus(403);
-        }
-      }
-    );
+
+  jsonwebtoken.verify(
+    token,
+    process.env.JWT_SECRET,
+    (err: JsonWebTokenError, decoded: JwtPayload) => {
+      if (err) res.sendStatus(403);
+
+      const userID = req.params.userID;
+      userID === undefined || userID === decoded.userID
+        ? next()
+        : res.sendStatus(403);
+    }
+  );
 };
 
 export default auth;
