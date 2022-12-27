2ffb2cc3d2aae3245b9df9b9855417982b0338f2
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-27 18:03:51 +0100

    added react-redux to dependencies and created store provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,14 +6,18 @@ import { CssBaseline } from "@mui/material";
 import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
 import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
 import theme from "./theme";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MUIThemeProvider theme={theme}>
-      <EmotionThemeProvider theme={theme}>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </EmotionThemeProvider>
-    </MUIThemeProvider>
+    <Provider store={store}>
+      <MUIThemeProvider theme={theme}>
+        <EmotionThemeProvider theme={theme}>
+          <CssBaseline />
+          <RouterProvider router={router} />
+        </EmotionThemeProvider>
+      </MUIThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
