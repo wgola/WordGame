2e400c5c886908590574760f79bf9bce5155bd10
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-27 18:36:15 +0100

    refactored NavBar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,5 +1,7 @@
 import { styled } from "@mui/material/styles";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { logout } from "../../utils/logout";
 import { Button } from "../Button";
 import { Tile } from "../Tile";
 
@@ -13,6 +15,7 @@ const StyledDiv = styled("div")`
 
 export const NavBar = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   return (
     <Tile width={1200}>
@@ -21,23 +24,28 @@ export const NavBar = () => {
           name="play"
           type="button"
           onClick={() => navigate("/home/play")}
+          disabled={loading}
         />
         <Button
           name="account"
           type="button"
           onClick={() => navigate("/home")}
+          disabled={loading}
         />
         <Button
           name="words list"
           type="button"
           onClick={() => navigate("/home")}
+          disabled={loading}
         />
         <Button
           name="logout"
           type="button"
-          onClick={() => {
-            navigate("/home");
+          onClick={async () => {
+            setLoading(true);
+            (await logout()) ? navigate("/login") : navigate("/home");
           }}
+          disabled={loading}
         />
       </StyledDiv>
     </Tile>
