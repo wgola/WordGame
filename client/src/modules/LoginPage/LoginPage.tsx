2e3138cde0b4131413ed286fd7c5fc95f1c69f90
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-28 16:01:41 +0100

    added redirection for logged user from login to home page

--- client/src/modules/LoginPage/LoginPage.tsx
+++ client/src/modules/LoginPage/LoginPage.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Tile } from "../../components/Tile";
+import { getUser, saveUserData } from "../../state/UserSlice";
+import { getUserData } from "../../utils";
+import { LoadingPage } from "../LoadingPage";
 import { LoginForm } from "./LoginForm";
 
 export const LoginPage = () => {
-  return (
+  const user = useSelector(getUser);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getUserData()
+      .then((res) => {
+        dispatch(saveUserData(res.data.userData));
+        navigate("/home");
+      })
+      .catch((err) => setLoading(false));
+  }, [user]);
+
+  return loading ? (
+    <LoadingPage />
+  ) : (
     <Tile width={500}>
       <h1>Login</h1>
       <LoginForm />
