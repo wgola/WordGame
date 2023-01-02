9f0c350acb5db550a221dc3065d5b7ad27823103
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-01-02 11:44:02 +0100

    Added dnd-provider to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,16 +8,20 @@ import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
 import theme from "./theme";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <MUIThemeProvider theme={theme}>
-        <EmotionThemeProvider theme={theme}>
-          <CssBaseline />
-          <RouterProvider router={router} />
-        </EmotionThemeProvider>
-      </MUIThemeProvider>
+      <DndProvider backend={HTML5Backend}>
+        <MUIThemeProvider theme={theme}>
+          <EmotionThemeProvider theme={theme}>
+            <CssBaseline />
+            <RouterProvider router={router} />
+          </EmotionThemeProvider>
+        </MUIThemeProvider>
+      </DndProvider>
     </Provider>
   </React.StrictMode>
 );
