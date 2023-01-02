abb3e0b47f4d5d225488f69b498c28eaf53a9c01
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-01-02 12:21:06 +0100

    created basic game class

---
+++ server/src/Game.ts
@@ -0,0 +1,20 @@
+import { Player } from "./types/Player";
+
+class Game {
+  gameID: string;
+  host: Player;
+  opponent: Player;
+
+  constructor(gameID: string, host: Player) {
+    this.gameID = gameID;
+    this.host = host;
+  }
+
+  setOpponent = (opponent: Player) => {
+    this.opponent = opponent;
+  };
+
+  toJson = () => {
+    return { gameID: this.gameID, host: this.host, opponent: this.opponent };
+  };
+}
---
+++ server/src/types/Player.ts
@@ -0,0 +1,4 @@
+export interface Player {
+  login: string;
+  color: string;
+}
