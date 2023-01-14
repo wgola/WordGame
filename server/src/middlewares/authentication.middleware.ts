4a69d41e4eae7ec3777390e5b48c5bd8e2397e36
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-01-14 15:11:50 +0100

    added logger to authentizaction middleware

--- server/src/middlewares/authentication.middleware.ts
+++ server/src/middlewares/authentication.middleware.ts
@@ -1,16 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import jsonwebtoken, { JsonWebTokenError, JwtPayload } from "jsonwebtoken";
+import log from "../configs/logs.config";
 
 const auth = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies["jwt-token"];
-  if (token === undefined) return res.sendStatus(401);
+
+  if (token === undefined) {
+    log.warn("Unauthorized user");
+    return res.sendStatus(401);
+  }
 
   jsonwebtoken.verify(
     token,
     process.env.JWT_SECRET,
     (err: JsonWebTokenError, decoded: JwtPayload) => {
-      if (err) return res.sendStatus(403);
+      if (err) {
+        log.warn("Unauthorized user");
+        return res.sendStatus(403);
+      }
 
+      log.info(`User ${decoded.toString()} authorized`);
       res.locals.userID = decoded.toString();
       return next();
     }
