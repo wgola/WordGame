e709dc51a4e86971244242817cc2b8e49591ea29
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-01-14 15:03:31 +0100

    added logger to words controller

--- server/src/routes/words.route.ts
+++ server/src/routes/words.route.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import auth from "../middlewares/authentication.middleware";
-import { post, get } from "../controllers/words.controller";
+import { get } from "../controllers/words.controller";
 
 const router = express.Router();
-
-router.post("/", auth, post);
 
 router.get("/", auth, get);
 
