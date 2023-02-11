1fdccf4060218c49dac688a4571257862bd4528d
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-02-11 17:48:35 +0100

    Added docker config for server, client and MQTT broker

--- client/src/mqtt.ts
+++ client/src/mqtt.ts
@@ -1,6 +1,9 @@
 import mqtt, { OnMessageCallback } from "precompiled-mqtt";
 
 const mqttConnect = () => {
+  const connection =
+    process.env.NODE_ENV === "development" ? "ws://mqtt-broker:9000/mqtt" : "";
+
   const client = mqtt.connect("ws://localhost:9000/mqtt");
 
   const publish = (topic: string, message: string) => {
